83dcebbd3ef75559aeb95877369518e303ab8047
Daniel Everett <deverett@gmail.com>
2024-05-01 21:37:59 +0000

    Add API for train expenses

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ A simple expense capturing PWA, using Google Sheets as the backend.
 | EXPENSER_SHEET_ID            | The ID of the Google Sheet backend. |
 | EXPENSER_AUTHNZ_DISABLED     | Disables authentication and authorization if set. |
 | EXPENSER_NO_SHEETS_API       | Disables interaction with Google Sheets API if set. |
+| EXPENSER_API_KEY             | The API key. |
--- api.go
+++ api.go
@@ -1,7 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func TrainApiHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNoContent)
+	apiKey := os.Getenv("EXPENSER_API_KEY")
+	reqApiKey := r.Header.Get("X-API-Key")
+
+	log.Println("Train API request")
+
+	if apiKey == "" {
+		http.Error(w, "API not configured", http.StatusServiceUnavailable)
+		log.Println("Train API not configured")
+		return
+	}
+	if reqApiKey != apiKey {
+		http.Error(w, "Invalid API key", http.StatusUnauthorized)
+		log.Println("Invalid API key")
+		return
+	}
+
+	expense := receivedData{
+		Category:    "Car",
+		Description: "Public transport",
+		Amount:      17.80,
+	}
+	err := appendExpense(expense, r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Sheets API call failed: %v\n", err)
+	} else {
+		w.WriteHeader(http.StatusNoContent)
+		log.Println("Submitted")
+	}
 }
